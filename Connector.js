72c1469e6af03fe96c133ef79f0e90a2e4772f04
Kwon <gene63@naver.com>
2019-10-06 13:29:25 +0900

    assignment complete

--- Connector.js
+++ Connector.js
@@ -2,7 +2,7 @@ const mariadb = require('mariadb');
 const pool = mariadb.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: 'gudrms63',
   database: 'testdb',
   connectionLimit: 5
 });
@@ -13,14 +13,17 @@ async function asyncFunction() {
     conn = await pool.getConnection();
     const rows = await conn.query('SELECT 1 as val');
     console.log(rows); //[ {val: 1}, meta: ... ]
+    console.log('end of 1 query');
     const res = await conn.query('INSERT INTO myTable value (?, ?)', [
       1,
       'mariadb'
     ]);
     console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+    console.log('end of 2 query');
   } catch (err) {
     throw err;
-  } finally {
+  } finally { 
+    console.log('end of function');
     if (conn) return conn.end();
   }
 }
