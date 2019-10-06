1668c9955924bfb2adf27a31c3920add2e8f96e9
Kwon <gene63@naver.com>
2019-10-06 22:04:56 +0900

    good solution

---
+++ Connector.js
@@ -0,0 +1,29 @@
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'testdb',
+  connectionLimit: 5
+});
+async function asyncFunction() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query('SELECT 1 as val');
+    console.log(rows); //[ {val: 1}, meta: ... ]
+    console.log('end of 1 query');
+    const res = await conn.query('INSERT INTO myTable value (?, ?)', [
+      1,
+      'mariadb'
+    ]);
+    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+    console.log('end of 2 query');
+  } catch (err) {
+    throw err;
+  } finally { 
+    console.log('end of function');
+    if (conn) return conn.end();
+  }
+}
+asyncFunction();
---
+++ InitSchema.js
@@ -0,0 +1,26 @@
+const mariadb = require('mariadb');
+const pool = mariadb.createPool
+({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    connectionLimit: 5
+});
+
+async function asyncFunction() {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        console.log("Initialized!");
+        await conn.query("create database testdb");
+        console.log("Create db");
+        await conn.query("use testdb");
+        await conn.query("create table myTable (number int, name varchar(255))");
+        console.log("Create table");
+    } catch (err) {
+      throw err;
+    } finally {
+      if (conn) return conn.end();
+    }
+}
+asyncFunction(); 
