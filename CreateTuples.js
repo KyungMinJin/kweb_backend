2a4b17fc5b1a1332889cec12cfbec5d589e20ae1
Kim gichan <geatrigger@gmail.com>
2019-10-30 16:26:13 +0900

    Make basic insert function
    should process error later

--- CreateTuples.js
+++ CreateTuples.js
@@ -11,21 +11,35 @@ const pool = mariadb.createPool
 });
 
 let create;
-ex.create = create = async function(tupleObject)
+ex.create = create = async function(table, tuple)
 {
     let conn, result;
+    const tables = {};
+    tables["member"] = ["id", "password", "class", "email", "name", "phone_number"];
+    tables["board"] = ["board_id", "board_class", "title", "content", "time", "id"];
     try
     {
-        /*
-        if(!())
-            return new Promise((resolve, reject)=>{
-                if(!result)
-                    reject(new Error("Error occured"));
-                resolve(result);
-            });
-            */
+        const values = [];
+        if(!(table == "member" || table == "board"))
+            throw new Error("no table");
+        for (const att of tables[table]) {
+            if(!tuple[att])
+            {
+                values.push("");
+                continue;
+            }
+            values.push(tuple[att]);
+        }
+        console.log("push done, values : ");
+        console.log(values);
         conn = await pool.getConnection();
         await conn.query("use kweb;");
+        console.log("use kweb");
+        if(table == "member")
+        {
+            result = await conn.query("insert into member values ?", [values]);
+            console.log("insert done");
+        }
     }
     catch (err)
     {
@@ -36,15 +50,21 @@ ex.create = create = async function(tupleObject)
         if (conn) 
         {
             await conn.end();
-            /*
+            
             return new Promise((resolve, reject)=>{
-                if(!result)
-                    reject(new Error("Error occured"));
                 resolve(result);
             });
-            */
+            
         }
     }
 }
 
 //read("board").then((res) => {console.log(res[0]);});
+const someTuple = {};
+someTuple.id = "myid"
+someTuple.password = "ED64384640CF857A66AB2888E3C9C4CED13B4C812BCBBDFAE7C61E83098C62E8" //kweb
+someTuple.class = 1;
+someTuple.email = "email@naver.com"
+someTuple.name = "tangtang";
+someTuple.phone_number = "01011112222";
+create("member", someTuple).then((res) => {console.log(res);});
